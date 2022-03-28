d7d3fc242a86aa17ed39d8891199ff7f90897105
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-28 19:25:36 +0200

    Add utility script to reinstall all binaries

---
+++ reinstall-all.sh
@@ -0,0 +1,6 @@
+cargo install --path lab1/exit-code
+cargo install --path lab1/paths
+cargo install --path lab1/show-all
+cargo install --path lab1/show-similar
+cargo install --path lab2/process
+cargo install --path lab2/head-tail
