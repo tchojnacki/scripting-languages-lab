d0fd4da1ced21cd783ea3b0802879eb6d490b71b
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-28 18:47:40 +0200

    Change arg parsing to match new specification

--- lab2/process/src/args.rs
+++ lab2/process/src/args.rs
@@ -4,25 +4,19 @@ use clap::{AppSettings, Parser};
 #[clap(setting = AppSettings::DeriveDisplayOrder, mut_arg("help", |h| h.hide(true)))]
 /// Parses stdin line by line using rules set by command arguments
 pub struct ProcessArgs {
-    #[clap(long, require_equals = true, value_name = "n")]
-    /// Skip first n characters in line
-    ignorefirst: Option<usize>,
+    #[clap(long, require_equals = true, value_name = "n", default_value_t = 0)]
+    /// Skip first n characters in each line
+    ignorefirst: usize,
 
-    #[clap(long, require_equals = true, value_name = "n")]
-    /// Skip last n characters in line
-    ignorelast: Option<usize>,
+    #[clap(long, require_equals = true, value_name = "n", default_value_t = 0)]
+    /// Skip last n characters in each line
+    ignorelast: usize,
 
-    #[clap(long, require_equals = true, value_name = "str")]
-    /// Replace all str occurrences with '\t'
-    delimiter: Option<String>,
+    #[clap(long, require_equals = true, value_name = "str", default_value = ",")]
+    /// Replace all str occurrences with sep
+    delimiter: String,
 
-    #[clap(
-        long,
-        require_equals = true,
-        value_name = "sep",
-        requires = "delimiter",
-        default_value = "\t"
-    )]
+    #[clap(long, require_equals = true, value_name = "sep", default_value = "\t")]
     /// Use custom replacement string for --delimiter
     separator: String,
 
@@ -33,7 +27,7 @@ pub struct ProcessArgs {
         value_name = "k,l,...,m"
     )]
     /// Display only columns k,l,...,m (after separation)
-    project: Vec<usize>,
+    project: Option<Vec<usize>>,
 
     #[clap(long, require_equals = true, value_name = "str")]
     /// Display only lines containing str
