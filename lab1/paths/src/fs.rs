59d6518b16f66865e829f840627fb6a82f35dd12
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-29 20:00:03 +0200

    Omit missing files instead of panicking

--- lab1/paths/src/fs.rs
+++ lab1/paths/src/fs.rs
@@ -1,51 +1,61 @@
-use std::{env, error::Error, fs, path::Path, time::SystemTime};
+use std::{env, fs, path::Path, time::SystemTime};
 
 use crate::{config::FileSorting, Config};
 
-pub fn traverse(config: Config) -> Result<(), Box<dyn Error>> {
-    recurse_directory(&env::current_dir()?, &config, 0)
+pub fn traverse(config: &Config) {
+    recurse_directory(
+        &env::current_dir().expect("Can not obtain current dir."),
+        config,
+        0,
+    )
 }
 
-fn recurse_directory(path: &Path, config: &Config, depth: usize) -> Result<(), Box<dyn Error>> {
-    let mut subpaths = fs::read_dir(path)?
-        .filter_map(Result::ok)
-        .map(|entry| entry.path())
-        .collect::<Vec<_>>();
+fn recurse_directory(path: &Path, config: &Config, depth: usize) {
+    if let Ok(dir) = fs::read_dir(path) {
+        let mut subpaths = dir
+            .filter_map(Result::ok)
+            .map(|entry| entry.path())
+            .collect::<Vec<_>>();
 
-    match config.sorting {
-        Some(FileSorting::Alpha) => subpaths.sort_by_key(|p| get_file_name(p).to_owned()),
-        Some(FileSorting::Date) => subpaths.sort_by_key(|p| get_modification_time(p).to_owned()),
-        None => (),
-    };
+        match config.sorting {
+            Some(FileSorting::Alpha) => subpaths.sort_by_key(|p| get_file_name(p).to_owned()),
+            Some(FileSorting::Date) => {
+                subpaths.sort_by_key(|p| get_modification_time(p).to_owned())
+            }
+            None => (),
+        };
 
-    for path in subpaths {
-        let file_name = get_file_name(&path);
+        for path in subpaths {
+            let file_name = get_file_name(&path);
 
-        if !config.directories_only || path.is_dir() {
-            let padding = config.indent.repeat(depth);
+            if !config.directories_only || path.is_dir() {
+                let padding = config.indent.repeat(depth);
 
-            print!("{padding}{file_name}");
+                print!("{padding}{file_name}");
 
-            if config.size_displayed && !path.is_dir() {
-                let file_size = fs::metadata(&path)?.len();
-                println!("\t{file_size}");
-            } else {
-                println!();
+                if config.size_displayed && !path.is_dir() {
+                    let file_size = get_file_size(&path);
+                    println!("\t{file_size}");
+                } else {
+                    println!();
+                }
+            }
+
+            if config.recursively && path.is_dir() {
+                recurse_directory(&path, config, depth + 1);
             }
         }
-
-        if config.recursively && path.is_dir() {
-            recurse_directory(&path, config, depth + 1)?;
-        }
     }
-
-    Ok(())
 }
 
 fn get_file_name(path: &Path) -> &str {
     path.file_name()
         .and_then(|name| name.to_str())
         .unwrap_or("")
+}
+
+fn get_file_size(path: &Path) -> u64 {
+    fs::metadata(&path).map(|data| data.len()).unwrap_or(0)
 }
 
 fn get_modification_time(path: &Path) -> SystemTime {
--- lab1/paths/src/main.rs
+++ lab1/paths/src/main.rs
@@ -1,12 +1,7 @@
 use clap::StructOpt;
 use paths::{traverse, Config};
-use std::process;
 
 fn main() {
     let config = Config::parse();
-
-    if let Err(e) = traverse(config) {
-        eprintln!("An error occured while reading the file system: {}", e);
-        process::exit(1);
-    }
+    traverse(&config);
 }
