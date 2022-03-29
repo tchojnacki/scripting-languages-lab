ea30ca03b5a58f2e36216b5aaeb405f83642a384
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-29 20:42:54 +0200

    Add error checking to longest-source-files

--- lab2/longest-source-files.sh
+++ lab2/longest-source-files.sh
@@ -1,5 +1,29 @@
 #!/bin/env bash
 set -euo pipefail
 
+# Validate script argument count
+if [[ "$#" -ne 2 ]]; then
+    echo "Invalid argument count! Exactly 2 required: directory and N."
+    exit 1
+fi
+
+# Validate first script argument
+if [[ ! -d "$1" ]]; then
+    echo "First argument must be a valid directory!"
+    exit 1
+fi
+
+# Validate second script argument
+if [[ ! "$2" =~ ^[[:digit:]]*$ ]]; then
+    echo "Second argument must be a non-negative number!"
+    exit 1
+fi
+
+# Paths works from current directory, change to requested dir
 cd "$1"
-paths -R -s | sed 's/^ *//g' | process --select=.rs --delimiter=$'\t\t' --separator=" " | sort -k2 -n -r | myhead --lines="$2"
+
+# Process paths output
+paths -Rs --indent="" |
+    process --select=.rs --delimiter=$'\t' --separator=" " |
+    sort -nrk2 |
+    myhead --lines="$2"
