a0da14a7fbc3ef1a27164ecb8365bbf98a6b5236
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-29 19:47:31 +0200

    Move script files to ~/bin on reinstall

--- lab2/longest-source-files.sh
+++ lab2/longest-source-files.sh
@@ -1,2 +1,4 @@
+#!/bin/env bash
+
 cd "$1"
 paths -R -s | sed 's/^ *//g' | process --select=.rs --delimiter=$'\t\t' --separator=" " | sort -k2 -n -r | myhead --lines="$2"
--- reinstall-all.sh
+++ reinstall-all.sh
@@ -1,6 +1,10 @@
+#!/bin/env bash
+
 cargo install --path lab1/exit-code
 cargo install --path lab1/paths
 cargo install --path lab1/show-all
 cargo install --path lab1/show-similar
 cargo install --path lab2/process
 cargo install --path lab2/head-tail
+cp lab1/exit-code-search.sh ~/bin/exit-code-search
+cp lab2/longest-source-files.sh ~/bin/longest-source-files
