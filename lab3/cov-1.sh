922415ce4d4b445980c0e4455d216bb802514a66
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-06 20:11:46 +0200

    Improve covid analysing scripts

--- lab3/cov-1.sh
+++ lab3/cov-1.sh
@@ -2,10 +2,8 @@ CONTINENT="Africa"
 YEAR=2020
 MONTH=8
 
-SEPARATOR=","
-
 cat covid.tsv |
     # 10,3,2,4 = continentExp,year,month,cases
-    process --delimiter=$'\t' --separator="$SEPARATOR" --project=10,3,2,4 --select="$CONTINENT,$YEAR,$MONTH," |
+    process --delimiter=$'\t' --separator=, --project=10,3,2,4 --select="$CONTINENT,$YEAR,$MONTH," |
     # 3 = cases
-    aggregate --aggr=sum --separator="$SEPARATOR" --column-index=3
+    aggregate --aggr=sum --separator=, --column-index=3
--- lab3/cov-2.sh
+++ lab3/cov-2.sh
@@ -1,15 +1,14 @@
+TAB=$'\t'
+
 COUNTRY="Poland"
 COUNT=2
 
-SEPARATOR=$'\t'
-
 cat covid.tsv |
     # 6,3,2,5 = countriesAndTerritories,year,month,deaths
-    process --delimiter=$'\t' --separator="$SEPARATOR" --project=6,3,2,5 --select="$COUNTRY," |
+    process --delimiter="$TAB" --project=6,3,2,5 --select="$COUNTRY$TAB" |
     # by columns 2 (year) then 3 (month)
-    sort -t "$SEPARATOR" -k2,2 -k3,3 |
+    sort -nk2,2 -nk3,3 |
     # group by 1 (year) and 2 (month), aggregate 3 (deaths)
-    aggregate --aggr=avg --separator="$SEPARATOR" --column-index=3 --group-indices=1,2 |
-    sort -t "$SEPARATOR" -nrk3 |
-    head --lines="$COUNT" |
-    process --delimiter="$SEPARATOR" --separator=$'\t'
+    aggregate --aggr=avg --column-index=3 --group-indices=1,2 |
+    sort -nrk3 |
+    head --lines="$COUNT"
